0f89024ea5378624404e70d9f4e590c5611042b4
Dominik <263915@student.pwr.edu.pl>
2024-03-25 11:52:27 +0100

    Add first signal working

--- main.py
+++ main.py
@@ -1,55 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_signal_from_file(file_path):
+
+def load_signal(filename):
     """
-    Wczytuje sygnał z pliku tekstowego.
-    Plik powinien zawierać wartości próbek sygnału EKG.
+    Funkcja wczytująca sygnał z pliku tekstowego.
     """
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    signal = []
-    for line in lines:
-        signal.append([float(x) for x in line.split()])
-    return np.array(signal)
-#kacper rakowski
-# Dominik 
+    data = np.loadtxt(filename)
+    return data
 
 
-def plot_signal(signal, time_scale=None, title=None, xlabel=None, ylabel=None):
+def plot_signal(data, fs, save_filename=None):
     """
-    Wizualizuje sygnał na wykresie.
+    Funkcja wizualizująca sygnał.
     """
+    time = np.arange(len(data)) / fs
+    print(data.shape[1])
+
     plt.figure(figsize=(10, 6))
-    if time_scale is not None:
-        plt.plot(time_scale, signal)
-    else:
-        plt.plot(signal)
-    if title is not None:
-        plt.title(title)
-    if xlabel is not None:
-        plt.xlabel(xlabel)
-    if ylabel is not None:
-        plt.ylabel(ylabel)
+    for i in range(data.shape[1]):
+        plt.plot(time, data[:, i], label=f'Odprowadzenie {i + 1}')
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Amplituda')
+    plt.title('Sygnały EKG z różnych odprowadzeń')
     plt.grid(True)
+    plt.legend()
+
+    if save_filename:
+        plt.savefig(save_filename)
+
     plt.show()
 
-def save_signal_to_file(signal, file_name):
-    """
-    Zapisuje sygnał do pliku o podanej nazwie.
-    """
-    np.savetxt(file_name, signal)
 
-# Przykładowe użycie funkcji:
-file_path = "ekg1.txt"
-file_path2 = "ekg100.txt"
-signal = load_signal_from_file(file_path2)
+# Wczytanie sygnału
+filename = "ekg1.txt"
+data = load_signal(filename)
 
-# Możliwość dostosowania osi czasu
-time_scale = np.arange(0, len(signal))  # Przykładowa skala czasu
-plot_signal(signal, time_scale, title="EKG Signal 1", xlabel="Czas [s]", ylabel="Amplituda")
+# Częstotliwość próbkowania
+fs = 1000
 
-# Możliwość zapisu wycinka sygnału do pliku
-fragment_signal = signal[100:200]  # Przykładowy wycinek sygnału
-plot_signal(fragment_signal, np.arange(100, 200), title="EKG Signal 2", xlabel="Czas [s]", ylabel="Amplituda")
-
+# Wizualizacja sygnału
+plot_signal(data, fs, save_filename="sygnaly_EKG.png")
