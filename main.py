02d74b66015e86d4f4afff5285b478cef00ca6fe
Dominik <263915@student.pwr.edu.pl>
2024-03-25 12:37:25 +0100

    add rest of the signals

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
+
 
 
 def load_signal(filename):
@@ -10,34 +13,54 @@ def load_signal(filename):
     return data
 
 
-def plot_signal(data, fs, save_filename=None):
+def plot_signal(data, fs, filename):
     """
     Funkcja wizualizująca sygnał.
     """
+
     time = np.arange(len(data)) / fs
-    print(data.shape[1])
 
     plt.figure(figsize=(10, 6))
-    for i in range(data.shape[1]):
-        plt.plot(time, data[:, i], label=f'Odprowadzenie {i + 1}')
+    if filename == "ekg100.txt":
+        plt.plot(time, data, label='Sygnał')
+    elif filename=="ekg_noise.txt":
+        plt.plot(data[:,0], data[:,1])
+    else:
+        for i in range(data.shape[1]):
+            plt.plot(time, data[:, i], label=f'Odprowadzenie {i + 1}')
     plt.xlabel('Czas [s]')
     plt.ylabel('Amplituda')
     plt.title('Sygnały EKG z różnych odprowadzeń')
     plt.grid(True)
-    plt.legend()
+    plt.legend(loc='upper right')
 
-    if save_filename:
-        plt.savefig(save_filename)
 
     plt.show()
 
 
-# Wczytanie sygnału
-filename = "ekg1.txt"
-data = load_signal(filename)
+def make_plot():
+    filename=file_combobox_1.get()
+    data = load_signal(filename)
+    if filename=="ekg1.txt":
+        fs=1000
+    else:
+        fs=360
 
-# Częstotliwość próbkowania
-fs = 1000
+    plot_signal(data, fs, filename)
 
-# Wizualizacja sygnału
-plot_signal(data, fs, save_filename="sygnaly_EKG.png")
+
+root = tk.Tk()
+root.title("Cyfrowe przedtwarzanie sygnałów i obrazów")
+
+file_list = ["ekg1.txt", "ekg100.txt", "ekg_noise.txt"]
+label_1 = ttk.Label(root, text="Select file")
+label_1.pack(pady=10)
+file_combobox_1 = ttk.Combobox(root, values=file_list, state="readonly", width=30)
+file_combobox_1.pack(pady=10)
+file_combobox_1.set("Select file")
+
+butto_1 = ttk.Button(root, text="Generate plot", command=make_plot)
+butto_1.pack(pady=10)
+
+root.mainloop()
+
